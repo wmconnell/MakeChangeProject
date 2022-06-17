61962dd9a701a5b33aea0689b7e6b0d2ce940890
wmconnell <wyatt.connell@gmail.com>
2022-06-17 19:56:11 -0400

    #1-3 complete; starting work on  #4

--- src/com/skilldistillery/makechange/MakeChange.java
+++ src/com/skilldistillery/makechange/MakeChange.java
@@ -1,9 +1,50 @@
 package com.skilldistillery.makechange;
 
+import java.util.Scanner;
+
 public class MakeChange {
 
 	public static void main(String[] args) {
-
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+		System.out.println("What is the price of the item?");
+		double price = scanner.nextInt();
+		
+		System.out.println("How much money did the customer give you?");
+		double payment = scanner.nextInt();
+		
+		calcPayment(price, payment);
+		
+		
 	}
 
+	private static void calcPayment(double price, double payment) {
+		double bill20 = 20;
+		double bill10 = 10;
+		double bill5 = 5;
+		double bill1 = 1;
+		double coin25 = 0.25;
+		double coin10 = 0.10;
+		double coin05 = 0.05;
+		double coin01 = 0.01;
+		double change = price - payment;
+		
+		if (price < payment) {
+			System.out.println("Change: " + change);
+			if (change < bill20 && change > bill10) {
+				
+			}
+		}
+		else if (price > payment) {
+			System.out.println("Insufficient payment.");
+			System.out.println("Ask customer to provide an additional $" + change + ", or insist customer seek better-paying employment.");
+		}
+		else {
+			System.out.println("Exact change provided. Thank you, come again!");
+			System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+		}
+	}
+
+	
 }
